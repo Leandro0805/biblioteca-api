34ac60e4e48b0889f619dcac2f82342a8d44925c
Leandro0805 <LeandroFilippini60@gmail.com>
2026-08-20 20:02:44 -0300

    Adição do arquivo schemas.py para o controle da entrada e saída de dados da API

---
+++ schemas.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel
+
+
+# Define os dados básicos de um livro
+class LivroBase(BaseModel):
+    titulo: str
+    autor: str
+
+
+# Define os dados necessários para criar um livro
+class LivroCreate(LivroBase):
+    ano_publicacao: int
+
+
+# Define os dados retornados pela API
+class LivroResponse(BaseModel):
+    id: int
+
+    class Config:
+        # Permite criar o schema a partir dos atributos de um objeto
+        from_attributes = True
+
+
+# Define os dados básicos de um empréstimo
+class BaseEmprestimo(BaseModel):
+    nome_leitor: str
+    data_emprestimo: int
+    devolvido: bool
+
+
+# Define os dados necessários para criar um empréstimo
+class CreateEmprestimo(BaseEmprestimo):
+    pass
+
+
+# Define os dados retornados pela API
+class ResponseEmprestimo(BaseEmprestimo):
+    livro_id: int
+
+    class Config:
+        # Permite criar o schema a partir dos atributos de um objeto
+        from_attributes = True
